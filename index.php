0d6a247ef79e6ba79127a8b78cd37b13f9613166
Daniel <schnolldj@gmail.com>
2019-06-06 21:39:23 -0400

    db

--- index.php
+++ index.php
@@ -12,27 +12,14 @@
     $active_group = 'default';
     $query_builder = TRUE;
 
-    $db['default'] = array(
-        'dsn'    => '',
-        'hostname' => $cleardb_server,
-        'username' => $cleardb_username,
-        'password' => $cleardb_password,
-        'database' => $cleardb_db,
-        'dbdriver' => 'mysqli',
-        'dbprefix' => '',
-        'pconnect' => FALSE,
-        'db_debug' => (ENVIRONMENT !== 'production'),
-        'cache_on' => FALSE,
-        'cachedir' => '',
-        'char_set' => 'utf8',
-        'dbcollat' => 'utf8_general_ci',
-        'swap_pre' => '',
-        'encrypt' => FALSE,
-        'compress' => FALSE,
-        'stricton' => FALSE,
-        'failover' => array(),
-        'save_queries' => TRUE
-    );
+    try {
+        $pdo = new PDO("mysql:host=".$cleardb_server."; dbname=".$cleardb_db, $cleardb_username, $cleardb_password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo = null;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
 ?>  
 <html>
     <head>
