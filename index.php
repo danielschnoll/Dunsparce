b639c833f880bf4c8c172bce73a792555a4ce386
Daniel <schnolldj@gmail.com>
2019-06-16 17:26:42 -0400

    cleaned up button style

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     $cleardb_db       = substr($cleardb_url["path"],1);
 
     try {
-        $conn = new PDO("mysql:host=localhost; dbname=dunsparce.net", "root", "");
-        //$conn = new PDO("mysql:host=".$cleardb_server."; dbname=".$cleardb_db, $cleardb_username, $cleardb_password);
+        //$conn = new PDO("mysql:host=localhost; dbname=dunsparce.net", "root", "");
+        $conn = new PDO("mysql:host=".$cleardb_server."; dbname=".$cleardb_db, $cleardb_username, $cleardb_password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
         print "Error!: " . $e->getMessage() . "<br/>";
